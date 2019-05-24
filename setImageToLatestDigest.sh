ad49df718e3455a53929fb189de07e94f587777d
Kenneth Holm Seelig <kenneth@reflective.dk>
2019-05-24 07:26:29 +0000

    add script

---
+++ setImageToLatestDigest.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+imageName=$1
+deployment=$1
+if [ $2 ]
+then
+  imageName=$2
+fi
+digest=`gcloud -q container images describe gcr.io/city-7337/${imageName}:latest --format="json" | jq ".image_summary.fully_qualified_digest" -r`
+echo $digest
+kubectl set image deployment $deployment *=$digest
